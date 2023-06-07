23f239353674b8e0c950227f8a53d89081b9d8db
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-06-07 09:31:42 +0200

    fix AOS on mobile
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,5 +2,7 @@
 // Use this file to add JavaScript to your project
 AOS.init({
     duration: 600,
-    easing: 'ease-in-sine'
+    easing: 'ease-in-sine',
+    // disable AOS on mobile completly
+    //disable: 'mobile'
 });
