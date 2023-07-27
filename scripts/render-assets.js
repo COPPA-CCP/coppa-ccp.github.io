53ef276df49bda17c7a52a89cb5652b4aaaa9f20
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-07-27 13:26:50 +0200

    add COPPA DID
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -6,6 +6,9 @@ const sh = require('shelljs');
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
+
+    const wellKnownPath = upath.resolve(upath.dirname(__filename), '../src/.well-known');
+
     sh.cp('-R', sourcePath, destPath)
+    sh.cp('-R', wellKnownPath, destPath)
 };
